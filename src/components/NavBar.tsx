8d7c74e5337fa820535cbb9f06fbdab133ead3cd
Prakash Mohaldar <prakash.mohaldar@sapiens.com>
2024-06-20 11:13:26 +0530

    google auth done

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,7 +1,12 @@
 import MaxWidthWrapper from "./MaxWidthWrapper"
 import Link from "next/link"
 import { buttonVariants } from "./ui/button"
-function NavBar() {
+import { getAuthSession } from "@/lib/auth"
+async function NavBar () {
+    const session = await getAuthSession()
+
+    console.log("--------------------------------------")
+    console.log(session)
   return (
     <nav className="sticky h-15 rounded inset-x-0 top-1 ml-2 mr-2 z-30 w-auto bg-white/5 backdrop-blur-lg transition-all">
         <MaxWidthWrapper>
@@ -16,9 +21,13 @@ function NavBar() {
                         variant: "ghost",
                         size: 'sm'
                     })}>Pricing </Link>
-                    <Link href='/sign-in' className={buttonVariants({
-                        size:'sm'
-                    })}>Sign-In </Link>
+
+                    {session? (<p>You are logged in</p>)
+                            : <Link href='/sign-in' className={buttonVariants({
+                                size:'sm'
+                            })}>Sign-In </Link>}
+
+               
 
                 </>
             </div>
